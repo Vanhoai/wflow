60754983979fba4140d70e8b187396f5b45d1f69
Tran Van Hoai <tvhoai241223@gmail.com>
2023-08-12 23:17:11 +0700

    config launch screen

--- android/app/src/main/kotlin/com/flow/wfow/MainActivity.kt
+++ android/app/src/main/kotlin/com/flow/wfow/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.flow.wfow
 
+import android.os.Build
 import android.os.Bundle
+import android.window.SplashScreenView
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
+
 
 class MainActivity: FlutterActivity() {
 
@@ -10,6 +14,14 @@ class MainActivity: FlutterActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            splashScreen.setOnExitAnimationListener { splashScreenView: SplashScreenView ->
+                splashScreenView.remove()
+            }
+        }
+
         super.onCreate(savedInstanceState)
     }
 

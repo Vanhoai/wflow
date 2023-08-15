56f5b1d3d93a73235b4bb9b3e2cdc698f17ff271
Tran Van Hoai <tvhoai241223@gmail.com>
2023-08-16 00:22:50 +0700

    update splash screen failed :v

--- android/app/src/main/kotlin/com/flow/wflow/MainActivity.kt
+++ android/app/src/main/kotlin/com/flow/wflow/MainActivity.kt
@@ -1,5 +1,17 @@
 package com.flow.wflow
 
+import android.os.Build
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {}
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            splashScreen.setOnExitAnimationListener { splashScreenView -> splashScreenView.remove() }
+        }
+
+        super.onCreate(savedInstanceState)
+    }
+}

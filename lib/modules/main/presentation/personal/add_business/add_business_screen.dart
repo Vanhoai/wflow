ad7a2914af02caee21e4466a52ca8d9f7b77cf88
Synxndh <ndhs824@gmail.com>
2023-10-14 00:32:30 +0700

    update: add business

--- lib/modules/main/presentation/personal/add_business/add_business_screen.dart
+++ lib/modules/main/presentation/personal/add_business/add_business_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:wflow/core/theme/colors.dart';
+import 'package:wflow/core/theme/size.dart';
+import 'package:wflow/configuration/constants.dart';
 import 'package:wflow/modules/main/presentation/personal/add_business/components/add_business_card.dart';
-import 'package:wflow/modules/main/presentation/personal/add_business/components/search_add_business.dart';
 import 'package:wflow/modules/main/presentation/personal/add_business/utils/constants.dart';
 
 class AddBusinessScreen extends StatefulWidget {
@@ -11,11 +14,39 @@ class AddBusinessScreen extends StatefulWidget {
 }
 
 class _AddBusinessScreenState extends State<AddBusinessScreen> {
-  void onCheck(value, index) {
+  TextEditingController controller = TextEditingController();
+  bool isHiddenSuffixIcon = true;
+  List<Map<String, dynamic>> foundUsers = users;
+
+  void onCheck(value, index) => setState(() {
+        users[index]['isCheck'] = value;
+      });
+
+  void onChangedSearch(String value) {
+    List<Map<String, dynamic>> result = [];
+
+    if (value.isEmpty) {
+      result = users;
+    } else {
+      result = users
+          .where(
+            (user) => user['name'].toString().toLowerCase().contains(
+                  value.toLowerCase(),
+                ),
+          )
+          .toList();
+    }
+
     setState(() {
-      users[index][3] = value;
+      isHiddenSuffixIcon = value.isEmpty;
+      foundUsers = result;
     });
   }
+
+  void onClearSearch() => setState(() {
+        isHiddenSuffixIcon = true;
+        controller.clear();
+      });
 
   @override
   Widget build(BuildContext context) {
@@ -29,16 +60,16 @@ class _AddBusinessScreenState extends State<AddBusinessScreen> {
         height: double.infinity,
         child: Column(
           children: [
-            const SearchAddBusiness(),
+            _buildSearchContainer(),
             Expanded(
               child: ListView.builder(
-                itemCount: users.length,
+                itemCount: foundUsers.length,
                 itemBuilder: (context, index) {
                   return AddBusinessCard(
-                    image: users[index][0],
-                    name: users[index][1],
-                    email: users[index][2],
-                    isCheck: users[index][3],
+                    image: foundUsers[index]['image'],
+                    name: foundUsers[index]['name'],
+                    email: foundUsers[index]['email'],
+                    isCheck: foundUsers[index]['isCheck'],
                     onCheck: (value) => onCheck(value, index),
                   );
                 },
@@ -47,6 +78,99 @@ class _AddBusinessScreenState extends State<AddBusinessScreen> {
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildSearchContainer() {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.only(
+        left: AppSize.paddingScreenDefault,
+        right: AppSize.paddingScreenDefault,
+        top: AppSize.paddingMedium * 2,
+        bottom: AppSize.paddingMedium * 2,
+      ),
+      child: _buildSearch(),
+    );
+  }
+
+  Widget _buildSearch() {
+    return SizedBox(
+      width: double.infinity,
+      height: 50,
+      child: TextField(
+        controller: controller,
+        onChanged: (value) => onChangedSearch(value),
+        decoration: InputDecoration(
+          hintText: 'Search',
+          hintStyle: const TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.normal,
+            color: AppColors.textGrey,
+          ),
+          prefixIcon: _buildPrefixIcon(),
+          suffixIcon: _buildSuffixIcon(),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(AppSize.borderMedium),
+            borderSide: const BorderSide(
+              width: 1,
+              color: AppColors.borderColor,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(AppSize.borderMedium),
+            borderSide: const BorderSide(
+              width: 1,
+              color: Colors.black54,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPrefixIcon() {
+    return Align(
+      widthFactor: 1,
+      heightFactor: 1,
+      child: SvgPicture.asset(
+        height: 20,
+        width: 20,
+        AppConstants.ic_search,
+        color: AppColors.borderColor,
+      ),
+    );
+  }
+
+  Widget _buildSuffixIcon() {
+    return Align(
+      widthFactor: 1,
+      heightFactor: 1,
+      child: isHiddenSuffixIcon
+          ? const SizedBox(
+              width: 0,
+              height: 0,
+            )
+          : InkWell(
+              onTap: () => onClearSearch(),
+              child: Container(
+                width: 20,
+                height: 20,
+                decoration: const BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      999,
+                    ),
+                  ),
+                ),
+                child: const Icon(
+                  Icons.close,
+                  size: 12,
+                  color: Colors.white,
+                ),
+              ),
+            ),
     );
   }
 }
--- lib/modules/main/presentation/personal/add_business/utils/constants.dart
+++ lib/modules/main/presentation/personal/add_business/utils/constants.dart
@@ -1,128 +1,51 @@
-final List users = [
-  [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
-    'Timdenmen Son',
-    'timdenmension@gmail.com',
-    false,
-  ],
-  [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
-    'Timdenmen Son',
-    'timdenmension@gmail.com',
-    false,
-  ],
-  [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
-    'Timdenmen Son',
-    'timdenmension@gmail.com',
-    false,
-  ],
-  [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
-    'Timdenmen Son',
-    'timdenmension@gmail.com',
-    false,
-  ],
-  [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
-    'Timdenmen Son',
-    'timdenmension@gmail.com',
-    false,
-  ],
-  [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
-    'Timdenmen Son',
-    'timdenmension@gmail.com',
-    false,
-  ],
-  [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
-    'Timdenmen Son',
-    'timdenmension@gmail.com',
-    false,
-  ],
-  [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
-    'Timdenmen Son',
-    'timdenmension@gmail.com',
-    false,
-  ],
-  [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
-    'Timdenmen Son',
-    'timdenmension@gmail.com',
-    false,
-  ],
-  [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
-    'Timdenmen Son',
-    'timdenmension@gmail.com',
-    false,
-  ],
-  [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
-    'Timdenmen Son',
-    'timdenmension@gmail.com',
-    false,
-  ],
-  [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
-    'Timdenmen Son',
-    'timdenmension@gmail.com',
-    false,
-  ],
-  [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
-    'Timdenmen Son',
-    'timdenmension@gmail.com',
-    false,
-  ],
-  [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
-    'Timdenmen Son',
-    'timdenmension@gmail.com',
-    false,
-  ],
-  [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
-    'Timdenmen Son',
-    'timdenmension@gmail.com',
-    false,
-  ],
-  [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
-    'Timdenmen Son',
-    'timdenmension@gmail.com',
-    false,
-  ],
-  [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
-    'Timdenmen Son',
-    'timdenmension@gmail.com',
-    false,
-  ],
-  [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
-    'Timdenmen Son',
-    'timdenmension@gmail.com',
-    false,
-  ],
-  [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
-    'Timdenmen Son',
-    'timdenmension@gmail.com',
-    false,
-  ],
-  [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
-    'Timdenmen Son',
-    'timdenmension@gmail.com',
-    false,
-  ],
-  [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
-    'Timdenmen Son',
-    'timdenmension@gmail.com',
-    false,
-  ],
+List<Map<String, dynamic>> users = [
+  {
+    'image':
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
+    'name': 'TimdenminSon',
+    'email': 'timdenminson@gmail.com',
+    'isCheck': false
+  },
+  {
+    'image':
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
+    'name': 'ai code ao day ta',
+    'email': 'timdenminson@gmail.com',
+    'isCheck': false
+  },
+  {
+    'image':
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
+    'name': 'sy ao ma canada',
+    'email': 'timdenminson@gmail.com',
+    'isCheck': false
+  },
+  {
+    'image':
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
+    'name': 'TimdenminSon',
+    'email': 'timdenminson@gmail.com',
+    'isCheck': false
+  },
+  {
+    'image':
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
+    'name': 'TimdenminSon',
+    'email': 'timdenminson@gmail.com',
+    'isCheck': false
+  },
+  {
+    'image':
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
+    'name': 'code chay roi',
+    'email': 'timdenminson@gmail.com',
+    'isCheck': false
+  },
+  {
+    'image':
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7b2PVyKI3rMsrNGIdognv6uTxYbDWYi8wAPbBgDa_cCmGU-r8qzIvRcqZTK1sQ_OpsqA&usqp=CAU',
+    'name': 'code hoat dong ngon',
+    'email': 'timdenminson@gmail.com',
+    'isCheck': false
+  },
 ];

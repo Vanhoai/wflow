3bb4d8643db40c17dcbdab787f88a80e7ffd7598
Kyna2k <huynobi1809@gmail.com>
2023-10-25 21:39:11 +0700

    clean code

--- lib/common/injection.dart
+++ lib/common/injection.dart
@@ -28,7 +28,6 @@ import 'package:wflow/modules/main/domain/cv/cv_repository.dart';
 import 'package:wflow/modules/main/domain/cv/cv_usercase.dart';
 import 'package:wflow/modules/main/domain/post/post_repository.dart';
 import 'package:wflow/modules/main/domain/post/post_usecase.dart';
-import 'package:wflow/modules/main/presentation/home/job/job_information/bloc/job_information_bloc/bloc.dart';
 
 import 'videocall/bloc/bloc.dart';
 

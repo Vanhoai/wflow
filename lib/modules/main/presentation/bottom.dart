2fb1058d2d423d48c985a9b49c6d5d183e9b4e8b
Synxndh <ndhs824@gmail.com>
2023-11-07 01:16:33 +0700

    update

--- lib/modules/main/presentation/bottom.dart
+++ lib/modules/main/presentation/bottom.dart
@@ -19,9 +19,7 @@ import 'package:wflow/core/theme/colors.dart';
 import 'package:wflow/core/widgets/shared/shared.dart';
 import 'package:wflow/modules/main/presentation/home/home/home.dart';
 import 'package:wflow/modules/main/presentation/message/rooms/rooms.dart';
-import 'package:wflow/modules/main/presentation/personal/add_business/add_business_screen.dart';
 import 'package:wflow/modules/main/presentation/personal/personal/personal.dart';
-import 'package:wflow/modules/main/presentation/personal/remove_collaborator/remove_collaborator_screen.dart';
 import 'package:wflow/modules/main/presentation/work/work/work.dart';
 
 class BottomNavigation extends StatefulWidget {
@@ -179,8 +177,8 @@ class _BottomNavigationState extends State<BottomNavigation>
         body: LazyLoadIndexedStack(
           index: currentIndex,
           children: [
-            const AddBusinessScreen(),
-            const RemoveCollaboratorScreen(),
+            const HomeScreen(),
+            const WorkScreen(),
             Container(),
             const RoomsScreen(),
             const PersonalScreen(),

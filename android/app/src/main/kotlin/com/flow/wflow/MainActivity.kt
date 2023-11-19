a2fe64a04ea8c96166927496d624e1848eda23c9
Aurora <hardy251223@gmail.com>
2023-11-19 08:14:44 +0700

    feat: pop stash git

--- android/app/src/main/kotlin/com/flow/wflow/MainActivity.kt
+++ android/app/src/main/kotlin/com/flow/wflow/MainActivity.kt
@@ -7,9 +7,7 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity: FlutterFragmentActivity() {
-    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
-
 }
